9cf6bdfc3f339c6f0d7352d45bcf4ab79217508b
hongpingliu <1320669338@qq.com>
2017-08-03 09:01:31 +0800

    String boot test

---
+++ src/test/java/per/liuhp/micserver/MicserverApplicationTests.java
@@ -0,0 +1,16 @@
+package per.liuhp.micserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
